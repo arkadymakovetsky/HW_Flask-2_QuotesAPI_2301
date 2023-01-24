2e270ef7548a413a477343817127ea481ceb804f
Arkady <arkadymakovetsky@gmail.com>
2023-01-24 20:16:01 +0300

    Change error message

--- ma_examples/schema.py
+++ ma_examples/schema.py
@@ -3,5 +3,5 @@ from marshmallow import Schema, fields
 
 class AuthorSchema(Schema):
     id = fields.Integer()
-    name = fields.Str(required=True, error_messages={'required': {'message': 'email is required', 'code': 400}})
+    name = fields.Str(required=True, error_messages={'required': {'message': 'name is required', 'code': 400}})
     email = fields.Email(required=True, error_messages={'required': 'email is required'})
